fd87f28a449c8388443ee7d864bcb48fb9959d7d
Josip Cavar <josipcavar04@gmail.com>
2024-05-07 10:08:43 +0200

    Update swift-shell to another upstream
    This fixes hang issue

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
     ] + developProducts,
     dependencies: [
         .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
-        .package(url: "https://github.com/taji-taji/swift-shell.git", from: "1.0.0"),
+        .package(url: "https://github.com/jcavar/swift-shell.git", branch: "main"),
         .package(url: "https://github.com/peripheryapp/periphery", from: "2.0.0"),
     ],
     targets: [
